5bf3afd3b633b91c23333ff5f1e3985e2763c19b
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2025-01-29 22:37:37 +0530

    exit - implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-
 void prompt()
 {
     std::cout << "$ ";
 
     std::string input;
     std::getline(std::cin, input);
-
+    if (input == "exit 0") {
+        exit(0);
+    }
     std::cout << input << ": command not found\n";
 }
 int main()
