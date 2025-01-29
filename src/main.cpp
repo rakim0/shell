d6a76565b620df56cd35801fc8fd82c7513f3e96
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2025-01-29 22:29:51 +0530

    REPL implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
+void prompt()
+{
+    std::cout << "$ ";
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::cout << input << ": command not found\n";
+}
 int main()
 {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // Uncomment this block to pass the first stage
-    std::cout << "$ ";
-
-    std::string input;
-    std::getline(std::cin, input);
-
-    std::cout << input << ": command not found";
+    while (1) {
+        prompt();
+    }
 }
