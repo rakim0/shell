b2719c5fc0c9d3feaa5e0f0127fa2b09f3943444
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2025-01-29 22:46:06 +0530

    echo implemented

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,16 @@ void prompt()
 
     std::string input;
     std::getline(std::cin, input);
-    if (input == "exit 0") {
-        exit(0);
+    if (input.find("exit ") != std::string::npos) {
+        int ret = stoi(input.substr(input.find("exit ") + 5));
+        if (ret == 0) {
+            exit(ret);
+        }
+    }
+    else if (input.find("echo ") != std::string::npos) {
+        std::string rest = input.substr(5);
+        std::cout << rest << '\n';
+        return;
     }
     std::cout << input << ": command not found\n";
 }
