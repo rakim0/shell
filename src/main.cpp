8faeaf1ee61fd1e13f7914d03b7b7a5ae89a5f28
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2025-01-29 23:17:27 +0530

    type done

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,47 @@
 #include <iostream>
+#include <set>
+#include <vector>
+
+void notFoundHandler(const std::string &command)
+{
+    std::cout << command << ": not found" << std::endl;
+    return;
+}
+void exitHandler(const std::string &input)
+{
+    std::string rest = input.substr(5);
+    if (rest.empty()) {
+        return;
+    }
+    int ret = stoi(rest);
+    exit(ret);
+}
+
+void echoHandler(const std::string &input)
+{
+    std::string rest = input.substr(5);
+    if (rest.empty()) {
+        return;
+    }
+    std::cout << rest << std::endl;
+}
+bool checkBuiltin(std::string command)
+{
+    static std::set<std::string> commands({"type", "echo", "exit"});
+    return commands.find(command) != commands.end();
+}
+void typeHandler(const std::string &input)
+{
+    std::string command = input.substr(5);
+    if (checkBuiltin(command)) {
+        std::cout << command << " is a shell builtin." << std::endl;
+    }
+    else {
+        notFoundHandler(command);
+    }
+    return;
+}
+
 void prompt()
 {
     std::cout << "$ ";
@@ -6,17 +49,17 @@ void prompt()
     std::string input;
     std::getline(std::cin, input);
     if (input.find("exit ") != std::string::npos) {
-        int ret = stoi(input.substr(input.find("exit ") + 5));
-        if (ret == 0) {
-            exit(ret);
-        }
+        exitHandler(input);
     }
     else if (input.find("echo ") != std::string::npos) {
-        std::string rest = input.substr(5);
-        std::cout << rest << '\n';
+        echoHandler(input);
         return;
     }
-    std::cout << input << ": command not found\n";
+    else if (input.find("type ") != std::string::npos) {
+        typeHandler(input);
+        return;
+    }
+    notFoundHandler(input);
 }
 int main()
 {
